9fc2524f0822da3359cbad5af9c1bc273a2b8c90
Simar Kalra <theramis@users.noreply.github.com>
2021-10-23 21:24:04 +1300

    Added node fetch

--- src/functions/dataRetriever.ts
+++ src/functions/dataRetriever.ts
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 const covidSiteUrl =
   "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-vaccine-data";
 
