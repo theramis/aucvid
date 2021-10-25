e0b1113047b093e58cd1d5d2361bf0c68e371ce8
Simar <theramis@users.noreply.github.com>
2021-10-25 19:41:31 +1300

    Convert app to nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,24 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Out of lockdown</title>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script src="https://cdn-tailwindcss.vercel.app/"></script>
-    <link href="./index.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="antialiased bg-pink-50 bg-opacity-50 h-full min-h-screen">
-      <div class="max-w-xl mx-5 sm:mx-auto py-20">
-        <section class="mb-12">
-          <div class="text-gray-900 text-lg md:text-2xl">Vaccination rates</div>
-          <h1 class="text-gray-900 text-4xl md:text-6xl font-bold">Auckland</h1>
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="antialiased bg-pink-50 bg-opacity-50 h-full min-h-screen">
+      <div className="max-w-xl mx-5 sm:mx-auto py-20">
+        <section className="mb-12">
+          <div className="text-gray-900 text-lg md:text-2xl">
+            Vaccination rates
+          </div>
+          <h1 className="text-gray-900 text-4xl md:text-6xl font-bold">
+            Auckland
+          </h1>
         </section>
         <section>
-          <h2 class="text-gray-900 text-lg md:text-2xl font-semibold mb-2">
+          <h2 className="text-gray-900 text-lg md:text-2xl font-semibold mb-2">
             Combined Auckland DHBs
           </h2>
-          <dl class="mb-6">
+          <dl className="mb-6">
             <div
-              class="
+              className="
                 w-44
                 flex flex-row
                 justify-between
@@ -28,9 +26,9 @@
                 text-green-500 text-lg
               "
             >
-              <dd class="w-32 flex items-center space-x-3">
+              <dd className="w-32 flex items-center space-x-3">
                 <div
-                  class="
+                  className="
                     w-5
                     h-5
                     flex
@@ -43,32 +41,32 @@
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
+                    className="h-4 w-4"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
                     data-src="https://heroicons.com/"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      stroke-lineCap="round"
+                      stroke-lineJoin="round"
+                      strokeWidth="2"
                       d="M5 13l4 4L19 7"
                     />
                   </svg>
                 </div>
                 <div>First dose</div>
               </dd>
-              <dt class="w-8">90%</dt>
+              <dt className="w-8">90%</dt>
             </div>
-            <div class="w-44 flex flex-row justify-between items-center">
-              <dd class="w-32">Second dose</dd>
-              <dt class="w-8">76%</dt>
+            <div className="w-44 flex flex-row justify-between items-center">
+              <dd className="w-32">Second dose</dd>
+              <dt className="w-8">76%</dt>
             </div>
           </dl>
-          <div class="w-full flex">
+          <div className="w-full flex">
             <div
-              class="
+              className="
                 w-full
                 relative
                 h-12
@@ -77,9 +75,9 @@
                 animate-fade-in
               "
             >
-              <div class="absolute inset-0 h-full w-[90%]">
+              <div className="absolute inset-0 h-full w-[90%]">
                 <div
-                  class="
+                  className="
                     bg-purple-400
                     h-full
                     rounded-lg
@@ -88,20 +86,22 @@
                   "
                 ></div>
               </div>
-              <div class="absolute inset-0 h-full w-[76%]">
+              <div className="absolute inset-0 h-full w-[76%]">
                 <div
-                  class="
+                  className="
                     bg-purple-500
                     h-full
                     rounded-lg
                     overflow-hidden
                     animate-grow-width
                   "
-                  style="animation-delay: 1s"
+                  // this breaks figure this out later
+                  style={{ animationDelay: "1s" }}
+                  // style="animation-delay: 1s"
                 ></div>
               </div>
               <div
-                class="
+                className="
                   absolute
                   top-0
                   left-[90%]
@@ -114,5 +114,7 @@
         </section>
       </div>
     </div>
-  </body>
-</html>
+  );
+};
+
+export default Home;
