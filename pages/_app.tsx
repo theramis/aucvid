375869d52789cd06dd005864b2fdbf38511e9962
Jishaal Kalyan <jishaal@gmail.com>
2021-10-26 22:16:51 +1300

    Add some opengraph tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/main.scss";
 
+const metaTags = {
+  title: "Auckland DHB Vaccination Rates to 90%",
+  description:
+    "Easily view Auckland's vaccinations to 90% by DHB and the daily progress towards getting out of lockdown.",
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -27,10 +33,22 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <title>Auckland DHB Vaccination Rates to 90%</title>
+        <title key="title">{metaTags.title}</title>
         <meta
           name="description"
-          content="Easily view Auckland's vaccinations to 90% by DHB and the daily progress towards getting out of lockdown."
+          content={metaTags.description}
+          key="description"
+        />
+        <meta
+          property="og:url"
+          content="https://outoflockdown.co"
+          key="ogUrl"
+        />
+        <meta property="og:title" content={metaTags.title} key="ogtitle" />
+        <meta
+          property="og:description"
+          content={metaTags.description}
+          key="ogdesc"
         />
       </Head>
       <Component {...pageProps} />
