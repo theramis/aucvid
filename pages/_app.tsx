f9a46db8a44addc77c7cc33cd73fed948e9f71ad
Jishaal Kalyan <jishaal@gmail.com>
2021-10-26 21:59:41 +1300

    Update meta tags and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,41 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/main.scss";
-import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Auckland DHB Vaccination Rates to 90%</title>
+        <meta
+          name="description"
+          content="Easily view Auckland's vaccinations to 90% by DHB and the daily progress towards getting out of lockdown."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
