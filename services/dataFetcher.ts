d4ba7be7138c0edad29a431211cf5da70ad8b49a
andycarrell <carrellandrew@gmail.com>
2021-10-27 20:59:44 +1300

    Adds 'has met target' to data fetcher

--- services/dataFetcher.ts
+++ services/dataFetcher.ts
@@ -3,6 +3,8 @@ import { HomePageProps } from "../pages";
 import { DhbData } from "../types/DhbData";
 import { DateTime } from "luxon";
 
+const firstDoseTargetPercentage = 0.9;
+const secondDoseTargetPercentage = 0.9;
 const cacheTtlInMins = 30;
 const covidSiteUrl =
   "https://www.health.govt.nz/our-work/diseases-and-conditions/covid-19-novel-coronavirus/covid-19-data-and-statistics/covid-19-vaccine-data";
@@ -79,12 +81,17 @@ function extractDhbData(rawHtml: string, dhbName: string): DhbData {
   );
   const c = dhbHtml![0].match(valuesRegex) as RegExpExecArray;
 
+  const firstDosesPercentage = convertToNumber(extractValue(c[1])) / 100;
+  const secondDosesPercentage = convertToNumber(extractValue(c[4])) / 100;
+
   return {
     numOfFirstDoses: convertToNumber(extractValue(c[0])),
-    firstDosesPercentage: convertToNumber(extractValue(c[1])) / 100,
+    firstDosesPercentage,
+    hasMetFirstDoseTarget: firstDosesPercentage >= firstDoseTargetPercentage,
     numOfFirstDosesTo90Percent: convertToNumber(extractValue(c[2])),
     numOfSecondDoses: convertToNumber(extractValue(c[3])),
-    secondDosesPercentage: convertToNumber(extractValue(c[4])) / 100,
+    secondDosesPercentage,
+    hasMetSecondDoseTarget: secondDosesPercentage >= secondDoseTargetPercentage,
     numOfSecondDosesTo90Percent: convertToNumber(extractValue(c[5])),
     totalPopulation: convertToNumber(extractValue(c[6])),
   };
@@ -99,9 +106,11 @@ const combineDhbData = (...data: DhbData[]): DhbData => {
   const combinedData: DhbData = {
     numOfFirstDoses: 0,
     firstDosesPercentage: 0,
+    hasMetFirstDoseTarget: false,
     numOfFirstDosesTo90Percent: 0,
     numOfSecondDoses: 0,
     secondDosesPercentage: 0,
+    hasMetSecondDoseTarget: false,
     numOfSecondDosesTo90Percent: 0,
     totalPopulation: 0,
   };
@@ -115,9 +124,13 @@ const combineDhbData = (...data: DhbData[]): DhbData => {
     combinedData.firstDosesPercentage = roundTo2Dp(
       combinedData.numOfFirstDoses / combinedData.totalPopulation
     );
+    combinedData.hasMetFirstDoseTarget =
+      combinedData.firstDosesPercentage > firstDoseTargetPercentage;
     combinedData.secondDosesPercentage = roundTo2Dp(
       combinedData.numOfSecondDoses / combinedData.totalPopulation
     );
+    combinedData.hasMetSecondDoseTarget =
+      combinedData.secondDosesPercentage > secondDoseTargetPercentage;
   });
 
   return combinedData;
