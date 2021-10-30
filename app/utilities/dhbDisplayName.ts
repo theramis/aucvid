47ec226f8692c75a3a6d2ab1fb7039e822c07343
andycarrell <carrellandrew@gmail.com>
2021-10-30 18:04:59 +1300

    Adds Waitematā, correctly spelled.

---
+++ app/utilities/dhbDisplayName.ts
@@ -0,0 +1,37 @@
+type NameMapType = {
+  [key: string]: string;
+};
+
+const nameMap: NameMapType = {
+  Auckland: "Auckland",
+  "Bay of Plenty": "Bay of Plenty",
+  Canterbury: "Canterbury",
+  "Capital and Coast": "Capital and Coast",
+  "Counties Manukau": "Counties Manukau",
+  "Hawkes Bay": "Hawkes Bay",
+  "Hutt Valley": "Hutt Valley",
+  Lakes: "Lakes",
+  MidCentral: "MidCentral",
+  "Nelson Malborough": "Nelson Malborough",
+  "Overseas / Unkown": "Overseas / Unkown",
+  Northland: "Northland",
+  "South Canterbury": "South Canterbury",
+  Southern: "Southern",
+  Tairawhiti: "Tairawhiti",
+  Taranaki: "Taranaki",
+  Waikato: "Waikato",
+  Wairarapa: "Wairarapa",
+  Waitemata: "Waitematā",
+  "West Coast": "West Coast",
+};
+
+const dhbDisplayName = (raw: string): string => {
+  console.log(raw, nameMap[raw], raw in nameMap);
+  if (raw in nameMap) {
+    return nameMap[raw];
+  }
+
+  return raw;
+};
+
+export default dhbDisplayName;
