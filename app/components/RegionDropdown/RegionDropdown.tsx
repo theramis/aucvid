64b262541cfa0d39e8deb22db3e01dffb094e631
Jishaal Kalyan <jishaal@gmail.com>
2021-10-30 22:43:00 +1300

    WIP styles for dropdown

--- app/components/RegionDropdown/RegionDropdown.tsx
+++ app/components/RegionDropdown/RegionDropdown.tsx
@@ -1,3 +1,5 @@
+import { ChevronDownIcon } from "@heroicons/react/outline";
+
 import { DhbRegionId } from "../../types/VaccineDataTypes";
 import styles from "./RegionDropdown.module.scss";
 
@@ -22,18 +24,25 @@ export const RegionDropdown = ({
   onChange,
 }: RegionDropdownProps) => {
   return (
-    <select
-      className={styles.dropdown}
-      value={selectedRegion}
-      onChange={(event) => onChange(event.target.value as DhbRegionId)}
-    >
-      {options.map((option: Option) => {
-        return (
-          <option key={option.id} value={option.id}>
-            {option.label}
-          </option>
-        );
-      })}
-    </select>
+    <div className={styles["dropdown-container"]}>
+      {/* <ChevronDownIcon className={styles["dropdown-caret"]} /> */}
+      <div className={styles["arrow"]}>
+        <span className={styles["left-arrow"]}></span>
+        <span className={styles["right-arrow"]}></span>
+      </div>
+      <select
+        className={styles["dropdown"]}
+        value={selectedRegion}
+        onChange={(event) => onChange(event.target.value as DhbRegionId)}
+      >
+        {options.map((option: Option) => {
+          return (
+            <option key={option.id} value={option.id}>
+              {option.label}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
