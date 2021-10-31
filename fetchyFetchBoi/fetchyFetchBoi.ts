8fec8e7fe1a2aef5f6e138535df61b2c1ebee777
Simar Kalra <theramis@users.noreply.github.com>
2021-10-31 17:18:17 +1300

    Send an alert in discord if Fetchy fails to fetch

--- fetchyFetchBoi/fetchyFetchBoi.ts
+++ fetchyFetchBoi/fetchyFetchBoi.ts
@@ -4,18 +4,34 @@ import {
   getDataForDate,
   updateDataIfDifferentForDate,
 } from "../shared/dataRepository";
+import got from "got";
 
 const main = async () => {
-  const vaccineData = await getVaccineData();
+  try {
+    const vaccineData = await getVaccineData();
 
-  const existingData = await getDataForDate(vaccineData.dataValidAsAtNzTimeIso);
-  if (existingData == null) {
-    await createDataForDate(vaccineData.dataValidAsAtNzTimeIso, vaccineData);
-  } else {
-    await updateDataIfDifferentForDate(
-      vaccineData.dataValidAsAtNzTimeIso,
-      vaccineData
+    const existingData = await getDataForDate(
+      vaccineData.dataValidAsAtNzTimeIso
     );
+    if (existingData == null) {
+      await createDataForDate(vaccineData.dataValidAsAtNzTimeIso, vaccineData);
+    } else {
+      await updateDataIfDifferentForDate(
+        vaccineData.dataValidAsAtNzTimeIso,
+        vaccineData
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    await got.post(process.env.DISCORD_WEBHOOK, {
+      json: {
+        avatar_url:
+          "https://i1.sndcdn.com/artworks-000497194077-gnjnca-t500x500.jpg",
+        content:
+          ":warning: :warning: **Fetchy ain't fetching!** :warning::warning:",
+        username: "Fetchy",
+      },
+    });
   }
 };
 
