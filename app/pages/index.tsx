1396362bb0890ada5bf70ea9290f974995af8a6c
andycarrell <carrellandrew@gmail.com>
2021-10-31 21:45:16 +1300

    Adds flex 0, applies to dark mode query, moves width to large only

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -35,7 +35,9 @@ const Home: React.FC<HomePageProps> = (props: HomePageProps) => {
                 <h2 className="heading-2">Vaccination rates</h2>
                 <h1 className="heading-1">Aotearoa</h1>
               </div>
-              {hasMounted && <DarkModeToggle className="margin-top-2xs" />}
+              {hasMounted && (
+                <DarkModeToggle className="flex-0 margin-top-2xs" />
+              )}
             </div>
             <RegionDropdown
               selectedRegion={region}
