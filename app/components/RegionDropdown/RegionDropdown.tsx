e6698a580cee22a744f47c6c23d9b91695c9b6bf
Jishaal Kalyan <jishaal@gmail.com>
2021-10-31 21:23:40 +1300

    Remove width/height altogether

--- app/components/RegionDropdown/RegionDropdown.tsx
+++ app/components/RegionDropdown/RegionDropdown.tsx
@@ -39,12 +39,7 @@ export const RegionDropdown = ({
       <div className={styles["dropdown-caret"]}>
         {/* <span className={styles["caret-left"]} />
         <span className={styles["caret-right"]} /> */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="100%"
-          height="100"
-          viewBox="0 0 24 24"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
         </svg>
       </div>
