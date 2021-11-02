63e073c8da32ab179d28306159f4c21e9107b6e5
Jishaal Kalyan <jishaal@gmail.com>
2021-11-02 19:15:15 +1300

    Refactor mapper + types

--- app/utilities/dhbDisplayName.ts
+++ app/utilities/dhbDisplayName.ts
@@ -1,35 +1,10 @@
 import { DhbName } from "../../shared/types/VaccineDataTypes";
 
-const nameMap: Record<DhbName, string> = {
-  Auckland: "Auckland",
-  "Bay of Plenty": "Bay of Plenty",
-  Canterbury: "Canterbury",
-  "Capital and Coast": "Capital and Coast",
-  "Counties Manukau": "Counties Manukau",
-  "Hawkes Bay": "Hawkes Bay",
-  "Hutt Valley": "Hutt Valley",
-  Lakes: "Lakes",
-  MidCentral: "MidCentral",
-  "Nelson Marlborough": "Nelson Marlborough",
-  "Overseas / Unknown": "Overseas / Unknown",
-  Northland: "Northland",
-  "South Canterbury": "South Canterbury",
-  Southern: "Southern",
+const nameMap: Partial<Record<DhbName, string>> = {
   Tairawhiti: "Tairāwhiti",
-  Taranaki: "Taranaki",
-  Waikato: "Waikato",
-  Wairarapa: "Wairarapa",
   Waitemata: "Waitematā",
-  "West Coast": "West Coast",
-  Whanganui: "Whanganui",
 };
 
-const dhbDisplayName = (raw: DhbName): string => {
-  if (raw in nameMap) {
-    return nameMap[raw];
-  }
-
-  return raw;
-};
+const dhbDisplayName = (raw: DhbName): string => nameMap[raw] ?? raw;
 
 export default dhbDisplayName;
