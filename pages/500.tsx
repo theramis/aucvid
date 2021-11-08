dcedce63ce9e75b36a43c111bbb31b3cb775b31e
andycarrell <carrellandrew@gmail.com>
2021-11-09 08:33:17 +1300

    Adds quick 500 page - hopefully will catch future errors

---
+++ pages/500.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+
+import { useHasMounted } from "../app/hooks/useIsMounted";
+import { Page, PageContainer, PageFooter } from "../app/components/Page";
+import { DarkModeToggle } from "../app/components/DarkModeToggle";
+import { InternalLink } from "../app/components/Link";
+
+const ServerError = () => {
+  const hasMounted = useHasMounted();
+
+  return (
+    <Page className="flex align-items-center">
+      <Head>
+        <title>Something went wrong | outoflockdown.co</title>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="googlebot" content="noindex, nofollow" />
+      </Head>
+      <section className="padding-top-4xl padding-bottom-l flex-1 width-full">
+        <PageContainer>
+          <div className="height-3xl flex flex-row justify-content-end">
+            {hasMounted && <DarkModeToggle className="flex-0" />}
+          </div>
+          <div className="flex flex-column align-items-center margin-y-5xl">
+            <h1 className="heading-1">500</h1>
+            <h2 className="heading-2 margin-bottom-l">Something went wrong</h2>
+            <InternalLink className="paragraph" href="/">
+              Back to homepage
+            </InternalLink>
+          </div>
+        </PageContainer>
+      </section>
+      <PageFooter />
+    </Page>
+  );
+};
+
+export default ServerError;
