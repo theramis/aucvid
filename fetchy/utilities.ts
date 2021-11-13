2004cc2e02d33db81b7b6e5d8a3463c2479de320
Simar <theramis@users.noreply.github.com>
2021-11-13 20:51:01 +1300

    Make fetchy alert if can't parse the data correctly

--- fetchy/utilities.ts
+++ fetchy/utilities.ts
@@ -1,2 +1,8 @@
-export const convertToNumber = (s: string): number =>
-  Number(s.replace(/[%,]/g, ""));
+export const convertToNumber = (s: string): number => {
+  const stripped = s.replace(/[%,]/g, "");
+  const result = Number(stripped || NaN);
+  if (Number.isNaN(result)) {
+    throw new Error(`Could not convert string ${s} to a number.`);
+  }
+  return result;
+};
